54dbc4f28d78b4fc270253c9c850e8f1703cf0bc
Daniel Schonfeld <danyschonfeld@gmail.com>
2017-08-21 12:31:36 -0700

    added an assertion

--- src/test/java/testSuites/TestLogin.java
+++ src/test/java/testSuites/TestLogin.java
@@ -1,5 +1,7 @@
 package testSuites;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import base.TestBase;
 
@@ -7,6 +9,7 @@ public class TestLogin extends TestBase {
 
 	@Test
 	public void loginTest() throws InterruptedException {
-		Thread.sleep(300);
+		driver.findElement(By.cssSelector("a[href='/sign_in']")).click();
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[type='submit']")).isDisplayed(), "User not redirected to Log In page");
 	}
 }
